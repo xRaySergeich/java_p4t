7c2dfd6c98db71ec29602c66e57a4e89becd345d
xRaySergeich <88889630+xRaySergeich@users.noreply.github.com>
2021-08-21 15:04:06 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/p4t/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/p4t/sandbox/MyFirstProgram.java
@@ -1,7 +1,25 @@
 package ru.p4t.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+
+    double len = 55;
+    double wid = 10;
+    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+    System.out.println("Площадь прямоугольника со сторонами " + len + ", " + wid + " = " + area(len, wid));
+  }
+
+  public  static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
 }

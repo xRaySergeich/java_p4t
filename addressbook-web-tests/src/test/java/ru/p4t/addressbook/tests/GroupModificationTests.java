c89adda4d9b86d8d7c86447f823ac43927f151b9
xRaySergeich <88889630+xRaySergeich@users.noreply.github.com>
2021-09-10 21:04:51 +0300

    добавлено сравнение объектов для модификации групп

--- addressbook-web-tests/src/test/java/ru/p4t/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/p4t/addressbook/tests/GroupModificationTests.java
@@ -4,22 +4,33 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.p4t.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
+    GroupData creationGroup = new GroupData("test1",null, null);
+
     app.getNavigationHelper().gotoGroupPage();
     if (!app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup(new GroupData("test1",null, null));
+      app.getGroupHelper().createGroup(creationGroup);
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(before.size() - 1);
+    int index = before.size() - 1;
+    app.getGroupHelper().selectGroup(index);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test4", "test5", "test6"));
+
+    GroupData modGroup = new GroupData(before.get(index).getId(), "test4", "test5", "test6");
+
+    app.getGroupHelper().fillGroupForm(modGroup);
     app.getGroupHelper().submitGroupModification();
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(index);
+    before.add(modGroup);
+    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
   }
 }

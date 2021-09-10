e8dc16f164ff207f43e147e753be1668b3438c53
xRaySergeich <88889630+xRaySergeich@users.noreply.github.com>
2021-09-10 21:35:11 +0300

    изменен спопоб поиска max id

--- addressbook-web-tests/src/test/java/ru/p4t/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/p4t/addressbook/tests/GroupCreationTests.java
@@ -13,19 +13,14 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1",null, null);
+    GroupData group = new GroupData("test2",null, null);
     app.getGroupHelper().createGroup(group);
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
+    int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
     group.setId(max);
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));

397b77b8b748650d64d94366788c0444eddd814f
xRaySergeich <88889630+xRaySergeich@users.noreply.github.com>
2021-09-10 21:16:32 +0300

    добавлено сравнение объектов при создании групп

--- addressbook-web-tests/src/test/java/ru/p4t/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/p4t/addressbook/model/GroupData.java
@@ -1,27 +1,26 @@
 package ru.p4t.addressbook.model;
 
 public class GroupData {
-  private final String id;
+
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
 
-    GroupData groupData = (GroupData) o;
-
-    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  public int getId() {
+    return id;
   }
 
-  @Override
-  public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public GroupData(int id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
   }
 
   @Override
@@ -32,19 +31,26 @@ public class GroupData {
             '}';
   }
 
-  public String getId() {
-    return id;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
-    this.id = id;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = -1;
     this.name = name;
     this.header = header;
     this.footer = footer;

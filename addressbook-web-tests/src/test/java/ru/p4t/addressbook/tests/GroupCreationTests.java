aa3c9a5e8591f9226381023395c98a936bb10f6e
xRaySergeich <88889630+xRaySergeich@users.noreply.github.com>
2021-09-26 16:12:08 +0300

    параметризован тест создания групп

--- addressbook-web-tests/src/test/java/ru/p4t/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/p4t/addressbook/tests/GroupCreationTests.java
@@ -4,16 +4,28 @@ import org.testng.annotations.*;
 import ru.p4t.addressbook.model.GroupData;
 import ru.p4t.addressbook.model.Groups;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<>();
+    list.add(new Object[] {new GroupData().withName("test 1").withHeader("header 1").withFooter("footer 1")});
+    list.add(new Object[] {new GroupData().withName("test 2").withHeader("header 2").withFooter("footer 2")});
+    list.add(new Object[] {new GroupData().withName("test 3").withHeader("header 3").withFooter("footer 3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test1");
     app.group().create(group);
     log.info("added group " + group);
     app.goTo().groupPage();

673f3b7dbed9fec568bdce10c3cfd9f6100394e4
xRaySergeich <88889630+xRaySergeich@users.noreply.github.com>
2021-09-26 20:56:00 +0300

    для тест создания контактов реализован дата провайдер из json файла данных + проверка на существование группы

--- addressbook-web-tests/src/test/java/ru/p4t/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/p4t/addressbook/tests/ContactCreationTests.java
@@ -1,47 +1,61 @@
 package ru.p4t.addressbook.tests;
 
+import com.google.gson.Gson;
+import org.openqa.selenium.json.TypeToken;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.p4t.addressbook.model.ContactData;
 import ru.p4t.addressbook.model.Contacts;
+import ru.p4t.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
-    File photo = new File("src/test/resources/anonymous.jpg");
-    ContactData cd = new ContactData()
-            .withFirstname("Zorian")
-            .withMiddlename("Viktorovich")
-            .withLastname("Kazinsky")
-            .withNickname("Nutcracker")
-            .withPhoto(photo)
-            .withCompany("Wizards, inc")
-            .withTitle("Some title")
-            .withAddress("Some address")
-            .withHomePhone("99922211")
-            .withMobilePhone("77718882")
-            .withWorkPhone("937557728")
-            .withFax("993949587")
-            .withEmail("address1@ex.com")
-            .withEmail2("address2@ex.com")
-            .withEmail3("address3@ex.com")
-            .withHomepage("http://localhost")
-            .withBday("10")
-            .withBmonth("August")
-            .withByear("1955")
-            .withAday("6")
-            .withAmonth("April")
-            .withAyear("1999")
-            .withGroup("test1")
-            .withAddress2("Some secondary address")
-            .withPhone2("888899999")
-            .withNotes("Extremely important notes for test");
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.json"));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType()); //List<ContactData>.class
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
 
+  private boolean isGroupExists = false;
+
+  @BeforeMethod
+  public void ensurePreconditions () {
+    if (!isGroupExists) {
+      app.goTo().groupPage();
+      if (app.group().all().size() == 0 || app.group().all().stream().filter(group -> group.getName().equals("test1")).findFirst().orElse(null) == null) {
+        GroupData creationGroup = new GroupData().withName("test1");
+        app.group().create(creationGroup);
+
+      }
+      isGroupExists = true;
+    }
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData cd) throws Exception {
+
+    File photo = new File("src/test/resources/anonymous.jpg");
+    cd = cd.withPhoto(photo);
     app.goTo().homePage();
 
     Contacts before = app.contact().all();

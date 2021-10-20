13fcb643681cb418e4e6e916760c33799f492dbb
xRaySergeich <88889630+xRaySergeich@users.noreply.github.com>
2021-10-20 22:54:19 +0300

    rest

---
+++ rest-sample/src/test/java/ru/p4t/rest/RestAssuredTest.java
@@ -0,0 +1,51 @@
+package ru.p4t.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static io.restassured.RestAssured.*;
+import static org.testng.Assert.assertEquals;
+
+public class RestAssuredTest {
+
+    @BeforeClass
+    public void init() {
+        authentication = basic("288f44776e7bec4bf44fdfeb1e646490", "");
+    }
+
+    @Test
+    public void testCreateIssue() throws IOException {
+        Set<Issue> oldIssues = getIssues();
+        Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
+        int issueId = createIssue(newIssue);
+        Set<Issue> newIssues = getIssues();
+        oldIssues.add(newIssue.withId(issueId));
+        assertEquals(newIssues, oldIssues);
+    }
+
+    private Set<Issue> getIssues() {
+        String json = get("https://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = JsonParser.parseString(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
+    }
+
+    private int createIssue(Issue newIssue) {
+        String body = String.format("subject=%s&description=%s", newIssue.getSubject(), newIssue.getDescription());
+        String json = given()
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .body(body)
+                .post("https://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = JsonParser.parseString(json);
+        return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+
+}
